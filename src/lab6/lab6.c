275e6ab67c322397fd7ecf940303521e421c287e
luong <gialuong9820@gmail.com>
2021-10-27 19:58:56 -0700

    update lab6

--- src/lab6/lab6.c
+++ src/lab6/lab6.c
@@ -4,7 +4,7 @@ void printCandidateReport ();
 
 void inMang(int *arr, int length){
   printf("[");
-  if(length==1)printf("%d",arr[0]);
+  if(length>=1)printf("%d",arr[0]);
   for(int i=1;i<length;i++){
       printf(",%d",arr[i]);
   }
@@ -18,16 +18,33 @@ int trongMang(int arr[], int length, int num){
   return -1;
 }
 
-int *xuatHienNhieuNhat(int arr[], int length){
-  int buffer[length][2];
+void xuatHienNhieuNhat(int arr[], int length){
+  int buffer[length];
+  int size[length];
+  int result[length];
   int buffLength=0;
   for(int i=0;i<length;i++){
     int check = trongMang(buffer,buffLength,arr[i]);
     if(check==-1){
+      buffer[buffLength]=arr[i];
+      size[buffLength]=1;
       buffLength++;
-      
+    }else{
+      size[check]++;
     }
   }
+  int max=0;
+  for(int i=0;i<buffLength;i++){
+    if(size[i]>max)max=size[i];
+  }
+  int resultLength=0;
+  for(int i=0;i<buffLength;i++){
+    if(size[i]==max){
+      result[resultLength]=buffer[i];
+      resultLength++;
+    }
+  }
+  inMang(result, resultLength);
 }
 
 int main(int argc, char *argv[]){
@@ -40,4 +57,5 @@ int main(int argc, char *argv[]){
     scanf("%d",&arr[i]);
   }
   printf("phan tu xuat hien nhieu nhat : ");
+  xuatHienNhieuNhat(arr,n);
 }
